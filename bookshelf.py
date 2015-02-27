134f4569ae847059a2f2e66ba7ba5557ec5556ef
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2015-02-27 22:09:36 +0500

    Simple form is added

--- bookshelf.py
+++ bookshelf.py
@@ -2,14 +2,28 @@ from flask import Flask
 from flask import request, render_template
 
 from flask.ext.script import Manager
+from flask.ext.wtf import Form
+
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField("Submit")
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "DumbAndDumbleDore"
+app.config["WTF_CSRF_ENABLED"] = True
 manager = Manager(app)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def Index():
-    # user_agent = request.headers.get("User-Agent")
-    return render_template("index.html")
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("index.html", form=form, name=name)
 
 @app.route("/user/<name>")
 def User(name):
