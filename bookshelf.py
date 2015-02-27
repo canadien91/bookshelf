ec27943786684c2cc0ad28a0817b62f7f0ffa5de
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2015-02-27 23:51:29 +0500

    First draft of the models

--- bookshelf.py
+++ bookshelf.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 from flask import request, render_template, session, redirect, url_for
 from flask import flash
 
 from flask.ext.script import Manager
 from flask.ext.wtf import Form
+from flask.ext.sqlalchemy import SQLAlchemy
 
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
@@ -12,10 +15,33 @@ class NameForm(Form):
     name = StringField("What is your name?", validators=[Required()])
     submit = SubmitField("Submit")
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "DumbAndDumbleDore"
 app.config["WTF_CSRF_ENABLED"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+
+db = SQLAlchemy(app)
 manager = Manager(app)
+
+class ARole(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship("User", backref="role")
+
+    def __repr__(self):
+        return "<ARole %r>" % self.name
+
+class AnUser(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+
+    def __repr__(self):
+        return "<AnUser %r>" % self.username
 
 @app.route("/", methods=["GET", "POST"])
 def Index():
