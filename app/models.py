780f99a532c6e005352279f6264ff95abf6779e6
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2015-02-28 08:55:09 +0500

    Password hashing

--- app/models.py
+++ app/models.py
@@ -1,21 +1,39 @@
 # coding: utf-8
 
-from . import db
+from werkzeug.security  import generate_password_hash
+from werkzeug.security  import check_password_hash
 
-class ARole(db.Model):
-    __tablename__ = "roles"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship("AnUser", backref="role", lazy="dynamic")
+from .                  import db
 
-    def __repr__(self):
+class ARole( db.Model ):
+    __tablename__   = "roles"
+
+    id              = db.Column( db.Integer, primary_key=True )
+    name            = db.Column( db.String( 64 ), unique=True )
+    users           = db.relationship( "AnUser", backref="role", lazy="dynamic" )
+
+    def __repr__( self ):
         return "<ARole %r>" % self.name
 
-class AnUser(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+class AnUser( db.Model ):
+    __tablename__   = "users"
+
+    id              = db.Column( db.Integer, primary_key=True )
+    username        = db.Column( db.String( 64 ), unique=True, index=True )
+    password_hash   = db.Column( db.String( 128 ) )
+    role_id         = db.Column( db.Integer, db.ForeignKey( "roles.id" ) )
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable attribute")
+
+    @password.setter
+    def password(self, value):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
 
     def __repr__(self):
         return "<AnUser %r>" % self.username
