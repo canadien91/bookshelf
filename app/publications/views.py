8663f382127296c73f9941b761ce0d4f8e235ffd
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2015-03-15 16:07:36 +0500

    Backend for Publication details

--- app/publications/views.py
+++ app/publications/views.py
@@ -1,12 +1,20 @@
-from flask import render_template, url_for
+from flask      import render_template, url_for
 
-from . import publications
-from ..models import APublication
+from .          import publications
+from ..models   import APublication
 
-@publications.route("/")
+@publications.route( "/" )
 def Index():
     publications_list = APublication.query.all()
     return render_template(
         "/publications/index.html",
         publications=publications_list,
+    )
+
+@publications.route( "/<int:pk>/" )
+def Details( pk ):
+    publication = APublication.query.get_or_404( pk )
+    return render_template(
+        "/publications/details.html",
+        publication=publication,
     )
